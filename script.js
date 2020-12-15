af1016ce587c7877362922c94b889258b73b624a
devih2001 <46088897+devih2001@users.noreply.github.com>
2020-12-16 01:31:41 +0700

    final commit

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
-let a = [];
+let checkAllowPositionQueen = [];
 let sizeChessBoard = 8;
 let totalSolution = [];
 let caseChange;
 let speedSolve = 500;
 let checkFirstRun = false;
 
-console.log("By Devih");
+console.log("By Devih From PTIT");
 
 let checkResult = (x2,y2) => {
     for(let i = 1; i < x2 ;i++)
-        if(a[i] == y2 || Math.abs(i-x2) == Math.abs(a[i] - y2) )
+        if(checkAllowPositionQueen[i] == y2 || Math.abs(i-x2) == Math.abs(checkAllowPositionQueen[i] - y2) )
             return false;
     return true;
 }
@@ -17,7 +17,7 @@ let checkResult = (x2,y2) => {
 let returnResult = (sizeChessBoard) => {
     let tempResult = [];
     for(let i = 1; i <= sizeChessBoard; i++){
-        tempResult.push(`${a[i]}${i}`)
+        tempResult.push(`${checkAllowPositionQueen[i]}${i}`)
     }
     totalSolution.push(tempResult);
 }
@@ -25,7 +25,7 @@ let returnResult = (sizeChessBoard) => {
 let recursionNextQueen = (i, sizeChessBoard) => {
     for(let j = 1; j <= sizeChessBoard ; j++){
         if(checkResult(i,j)){
-            a[i] = j;
+            checkAllowPositionQueen[i] = j;
             if(i === sizeChessBoard ) {
               returnResult(sizeChessBoard);
             }
@@ -47,7 +47,7 @@ let stopSolve = () => {
     checkFirstRun = false;
 }
 
-let changePositonQueen = () => {
+let changePositionQueen = () => {
     let checkEnd = 0;
     checkFirstRun = true;
 
@@ -72,7 +72,7 @@ let changeContentButton = () => {
     document.getElementById("speed").innerHTML = document.getElementById("myRange").value;
     speedSolve = parseInt(document.getElementById("myRange").value);
     if(checkFirstRun){
-        changePositonQueen();
+        changePositionQueen();
     }
 }
 
